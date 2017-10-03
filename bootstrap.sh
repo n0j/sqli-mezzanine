2cae5b0774406569c37298651097ded32d0eb871
Mike McCambridge <mccambrm@gmail.com>
2017-10-02 21:43:13 -0700

    reorder bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,13 @@
 MYSQL_PASS='JUxdH8ZNQmPLYtb'
 D=`dirname $0`
 
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASS"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASS"
+
 apt-get update
 apt-get install -y apache2
 apt-get install -y mysql-server
 apt-get install -y php libapache2-mod-php php-mcrypt php-mysql
-
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASS"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASS"
 
 cat << EOF > /etc/apache2/mods-enabled/dir.conf
 <IfModule mod_dir.c>
